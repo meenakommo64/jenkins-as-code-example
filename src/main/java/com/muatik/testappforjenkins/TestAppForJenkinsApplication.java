f3421c9dafd5e53601f719591ff9ce1be57cff32
Mustafa Atik <muatik@gmail.com>
2018-02-05 11:39:23 +0100

    maven projects is added to test build pipeline

---
+++ src/main/java/com/muatik/testappforjenkins/TestAppForJenkinsApplication.java
@@ -0,0 +1,19 @@
+package com.muatik.testappforjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TestAppForJenkinsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestAppForJenkinsApplication.class, args);
+    }
+
+    @GetMapping
+    public String greetigs() {
+        return "Hello, it works. That's all.";
+    }
+}
